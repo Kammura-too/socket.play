8e7f948cc56778976ceea03b2b48cd22e174ea21
Kammura-too <146385509+Kammura-too@users.noreply.github.com>
2024-05-26 02:04:36 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const port = process.env.PORT || 8080;
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('message', msg);
+    });
+});
+
+server.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${port}`);
+});
